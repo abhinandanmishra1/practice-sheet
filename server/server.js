9b306e02250826cd8f222a9915270461346a0f40
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2023-12-08 09:17:35 +0000

    fix: env variable

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const problemRoutes = require("./Routes/ProblemRoutes");
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: process.env.CLIENT_URL || "http://localhost:5173"
 }));
 
 app.listen(PORT, async () => {
