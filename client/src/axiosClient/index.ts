2fb6a15c09c8d5a08558ed73c96c1aa33d4562be
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2023-12-08 11:10:13 +0530

    Update index.ts

--- client/src/axiosClient/index.ts
+++ client/src/axiosClient/index.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_URL || "https://practice-sheet.vercel.app",
     timeout: 5000
 })
